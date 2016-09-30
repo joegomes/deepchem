0722c404399631668ce3880a4c9a2bb2acd11b23
joegomes <jgomes@berkeley.edu>
2016-09-30 13:50:31 -0700

    Cleaning up branch

--- deepchem/featurizers/atomic_coordinates.py
+++ deepchem/featurizers/atomic_coordinates.py
@@ -51,8 +51,7 @@ class AtomicCoordinates(Featurizer):
 
 class FixedSizeAtomicCoordinates(Featurizer):
   """
-  1x3 Header (num_atoms, 0, 0)
-  Nx3 matrix of Cartesian coordinates [Bohr]
+  Nx4 matrix of Cartesian coordinates [Bohr] and masking value
   """
   name = ['atomic_coordinates']
 
@@ -74,7 +73,7 @@ class FixedSizeAtomicCoordinates(Featurizer):
 
     N = mol.GetNumAtoms()
     Nmax = self.max_atoms
-    coords = np.zeros((Nmax+1,3))
+    coords = np.zeros((Nmax,3))
 
     # RDKit stores atomic coordinates in Angstrom. Atomic unit of length is the
     # bohr (1 bohr = 0.529177 Angstrom). Converting units makes gradient calculation
@@ -82,11 +81,51 @@ class FixedSizeAtomicCoordinates(Featurizer):
     coords_in_bohr = [mol.GetConformer(0).GetAtomPosition(i).__div__(0.52917721092)
                       for i in xrange(N)]
 
-    coords[0,0] = N
     for atom in xrange(N):
-      coords[atom+1,0] = coords_in_bohr[atom].x
-      coords[atom+1,1] = coords_in_bohr[atom].y
-      coords[atom+1,2] = coords_in_bohr[atom].z
+      coords[atom,0] = coords_in_bohr[atom].x
+      coords[atom,1] = coords_in_bohr[atom].y
+      coords[atom,2] = coords_in_bohr[atom].z
+
+    coords = [coords]
+    return coords
+
+class VariadicAtomicCoordinates(Featurizer):
+  """
+  Nx4 matrix of Cartesian coordinates [Bohr] and masking value
+  """
+  name = ['atomic_coordinates']
+
+  def __init__(self, max_atoms=None):
+    # Type of data created by this featurizer
+    self.dtype = object
+    self.max_atoms = max_atoms
+    
+
+  def _featurize(self, mol):
+    """
+    Calculate atomic coodinates.
+
+    Parameters
+    ----------
+    mol : RDKit Mol
+          Molecule.
+    """
+
+    N = mol.GetNumAtoms()
+    Nmax = self.max_atoms
+    coords = np.zeros((Nmax,4))
+
+    # RDKit stores atomic coordinates in Angstrom. Atomic unit of length is the
+    # bohr (1 bohr = 0.529177 Angstrom). Converting units makes gradient calculation
+    # consistent with most QM software packages.
+    coords_in_bohr = [mol.GetConformer(0).GetAtomPosition(i).__div__(0.52917721092)
+                      for i in xrange(N)]
+
+    for atom in xrange(N):
+      coords[atom,0] = coords_in_bohr[atom].x
+      coords[atom,1] = coords_in_bohr[atom].y
+      coords[atom,2] = coords_in_bohr[atom].z
+      coords[atom,3] = 1.0
 
     coords = [coords]
     return coords
